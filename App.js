28c2b96d502e3bdcb454d704159ec16bf42a6b33
Shreekumar Hegde <shreekumarhegde@Shreekumars-MacBook-Air.local>
2025-10-20 12:20:59 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ])
+);
+const heading = React.createElement("h1", {}, "Hello, React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
