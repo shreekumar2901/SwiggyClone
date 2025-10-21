3570e3bf5d865943f0fe547118b1e78df8270256
Shreekumar Hegde <s.shreekumar.c@gmail.com>
2025-10-21 09:11:24 +0530

    Move react and react-dom to dependency in package.json

--- App.js
+++ App.js
@@ -1,11 +1,6 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ])
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", {}, "Hello, React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading);
