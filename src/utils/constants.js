040b57f50785235ae7e75691b0ecf3bd982a59a8
Shreekumar Hegde <s.shreekumar.c@gmail.com>
2025-10-27 12:12:35 +0530

    Use a local backend to resolve CORS issue

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,5 +4,5 @@ const LOGO_URL =
   "https://1000logos.net/wp-content/uploads/2021/05/Swiggy-logo-1536x864.png";
 const RESTAURANT_API =
   "https://www.swiggy.com/dapi/restaurants/search/v3?lat=13.0035068&lng=77.5890953&str=North%20Indian&trackingId=eb47e2be-0d25-0adb-5e6d-b1e377cb7c65&submitAction=ENTER&queryUniqueId=4c0cb329-a213-be5b-d57f-06a40609ee13";
-const PROXY_URI = "https://proxyy-7bi7.onrender.com/";
+const PROXY_URI = "http://localhost:3001/";
 export { CDN_URL, LOGO_URL, RESTAURANT_API, PROXY_URI };
