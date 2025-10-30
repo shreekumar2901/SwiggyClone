a48b9a96f49429b929fa563039b911517e9723b9
Shreekumar Hegde <s.shreekumar.c@gmail.com>
2025-10-30 09:17:20 +0530

    Add navigation from restaurant list to menu

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,30 +1,33 @@
+import { NavLink } from "react-router-dom";
 import { CDN_URL } from "../utils/constants";
 
 const RestaurantCard = ({ restaurant }) => {
   return (
-    <div className="res-card">
-      <img
-        className="res-logo"
-        src={CDN_URL + restaurant.info.cloudinaryImageId}
-        alt="res-logo"
-      />
-      <h3>{restaurant.info.name}</h3>
-      <h4>{restaurant.info.cuisines.join(", ")}</h4>
-      <div className="res-meta">
-        <span className="res-meta-item res-meta-rating">
-          <span className="res-meta-icon">★</span>
-          {restaurant.info.avgRating}
-        </span>
-        <span className="res-meta-item res-meta-price">
-          <span className="res-meta-icon">₹</span>
-          {restaurant.info.costForTwo}
-        </span>
-        <span className="res-meta-item res-meta-time">
-          <span className="res-meta-icon">⏱</span>
-          {restaurant.info.sla.deliveryTime} mins
-        </span>
+    <NavLink to={`/restaurant/${restaurant.info.id}`}>
+      <div className="res-card">
+        <img
+          className="res-logo"
+          src={CDN_URL + restaurant.info.cloudinaryImageId}
+          alt="res-logo"
+        />
+        <h3>{restaurant.info.name}</h3>
+        <h4>{restaurant.info.cuisines.join(", ")}</h4>
+        <div className="res-meta">
+          <span className="res-meta-item res-meta-rating">
+            <span className="res-meta-icon">★</span>
+            {restaurant.info.avgRating}
+          </span>
+          <span className="res-meta-item res-meta-price">
+            <span className="res-meta-icon">₹</span>
+            {restaurant.info.costForTwo}
+          </span>
+          <span className="res-meta-item res-meta-time">
+            <span className="res-meta-icon">⏱</span>
+            {restaurant.info.sla.deliveryTime} mins
+          </span>
+        </div>
       </div>
-    </div>
+    </NavLink>
   );
 };
 export default RestaurantCard;
