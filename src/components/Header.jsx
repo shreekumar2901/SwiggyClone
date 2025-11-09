ea9631075bbaf0628a0ce7a9510f64a0b30834d5
Shreekumar Hegde <s.shreekumar.c@gmail.com>
2025-11-09 07:49:58 +0530

    Installed tailwind and changed Header component to tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,31 @@
-import { NavLink } from "react-router-dom";
 import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="header">
+    <div className="flex items-center justify-between py-3 px-7 bg-white border border-gray-200 rounded-b-2xl rounded-t-none shadow-lg">
       <Link to="/">
-        <div className="logo-container">
-          <img className="logo" src={LOGO_URL} alt="logo" />
+        <div className="flex items-center gap-2">
+          <img className="w-24 drop-shadow-logo" src={LOGO_URL} alt="logo" />
         </div>
       </Link>
-      <ul className="nav-items">
-        <li>
+      <ul className="list-none flex ml-auto  gap-[18px] items-center p-0 nav-items">
+        <li className="nav-items-li">
           <Link className="nav-item" to="/">
             Home
           </Link>
         </li>
-        <li>
+        <li className="nav-items-li">
           <Link className="nav-item" to="/about">
             About
           </Link>
         </li>
-        <li>
+        <li className="nav-items-li">
           <Link className="nav-item" to="/contact">
             Contact
           </Link>
         </li>
-        <li>Cart</li>
+        <li className="nav-items-li">Cart</li>
       </ul>
     </div>
   );
